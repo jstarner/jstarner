bd092cfc60833b75a8574af172dfb48b35f119e9
jstarner <justinstarner@Polars-MacBook-Pro.local>
2017-12-11 22:01:53 -0500

    Implemented dynamic pages for projects index and project view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :projects
+
+  namespace :admin do
+    resources :projects, except: [ :show ]
+  end
+
   get :index, controller: :pages
   get :projectindex, controller: :pages
   get :projectview, controller: :pages
   get :resume, controller: :pages
   get :about, controller: :pages
   get :faq, controller: :pages
+
+  resources :projects, only: [ :index, :show ]
 
   root 'pages#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,11 @@ Project.create(
   long_desc: 'This was a team project that that was created using HTML, CSS, SQL, Javascript, Bootstrap, and PHP. Our team populated our database using the IMDb API.', 
   project_link: 'https://watchflix.000webhostapp.com/index.php'
   )
+
+Project.create(
+  title: 'Black Jack',
+  image: 'blackjack.png',
+  short_desc: 'A ruby program that allows a user to play black jack from within the terminal',
+  long_desc: 'This was the first project I completed during my Ruby on Rails apprenticeship. This program required just under 300 lines of code and necessitated the use of many special methods in order to achieve proper functionality', 
+  project_link: 'https://watchflix.000webhostapp.com/index.php'
+  )
